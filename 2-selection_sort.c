c8a1b033978f84cd7524ac18f4e3cf4c88c7ee30
Mohammed <mrcloud.med@gmail.com>
2024-02-19 09:47:15 -0800

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,9 +9,10 @@
 */
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+
+	*xp = *yp;
+	*yp = temp;
 }
 
 /**
@@ -24,18 +25,18 @@ void swap(int *xp, int *yp)
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+	size_t i, j, min_idx;
 
-    for (i = 0; i < size-1; i++)
-    {
-        min_idx = i;
-        for (j = i+1; j < size; j++)
-          if (array[j] < array[min_idx])
-            min_idx = j;
-           if(min_idx != i)
-           {
-            swap(&array[min_idx], &array[i]);
-            print_array(array, size);
-           }
-    }
+	for (i = 0; i < size-1; i++)
+	{
+		min_idx = i;
+		for (j = i+1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		if(min_idx != i)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
