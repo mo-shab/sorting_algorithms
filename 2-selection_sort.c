ec55be5c04196b2db4abffcb173dd3123c302414
Mohammed <mrcloud.med@gmail.com>
2024-02-19 10:36:36 +0100

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * swap - function that swap.
+ * @xp: First parametre
+ * @yp: second param
+ * 
+ * Return: Nothing
+*/
+void swap(int *xp, int *yp)
+{ 
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+/**
+ * selection_sort - Function implement algorithme selection sort
+ * @array: The array to be sorted
+ * @size: The size of the array.
+ * 
+ * Retrun: Nothing
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    int i, j, min_idx;
+  
+    for (i = 0; i < size-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < size; j++)
+          if (array[j] < array[min_idx])
+            min_idx = j;
+           if(min_idx != i)
+           {
+            swap(&array[min_idx], &array[i]);
+            print_array(&array, size);
+           }
+            
+    } 
+}
